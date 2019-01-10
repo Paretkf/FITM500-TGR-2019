a39718fc930a63fe8b8c58438ab41cec33516c08
Paretkf <paretk7@gmail.com>
2019-01-10 11:49:24 +0700

    change train model

--- Line_ML/Day2_RNN/testRead.js
+++ Line_ML/Day2_RNN/testRead.js
@@ -5,7 +5,7 @@ async function main () {
   let data = await read()
   const tensorFlow = new Tensorflow(data, 7)
   await tensorFlow.trainDataScaling()
-  await tensorFlow.predictDataScaling([[32.95, 32.845, 32.861, 32.94, 32.99, 32.89, 32.98]], 35)
+  await tensorFlow.predictDataScaling([[32.95, 32.845, 32.861, 32.94, 32.99, 32.89, 32.98]])
 }
 
 main();
